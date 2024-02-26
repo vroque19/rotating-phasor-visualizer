c686723610650bb66fc177c672d821e0673a0f77
Vanessa Isabel Roque <vroque19@csu.fullerton.edu>
2024-02-25 21:58:08 -0800

    draw waves

--- static/js/sketch.js
+++ static/js/sketch.js
@@ -2,6 +2,9 @@ let angle = 0;
 let theta = 0;
 let t;
 let amplitude = 0;
+let cos_points = [];
+let sin_points = [];
+let frame_count = 0;
 
 function setup() {
     createCanvas(950, 700);
@@ -12,12 +15,12 @@ function setup() {
     angleMode(RADIANS);
     noFill();
 
-
 }
 
 function draw() {
     background(29);
-    // make center of screen our starting point
+    frame_count ++;
+    // make center of screen  starting point
     translate(w/2, h/2);
     drawAxes()
     amplitude = 75; // radius == amplitude
@@ -26,31 +29,61 @@ function draw() {
     theta = angle * t;
     y = amplitude * sin(-theta); // y = r sinΘ
     x = amplitude * sin(theta + PI/2); // x = r cosΘ
+    drawVector(y, x);
     drawOscillations(y, x);
-    drawRadius(y, x);
-    drawSignal(t, y, x);
     circle(0, 0, 2 * amplitude);
+    drawSignal(t, y, x);
 }
 
 function drawSignal(t, y2, y1) {
-    point(t, y2);
-    point(y1, t);
+    push();
+    stroke("pink");
+    strokeWeight(2);
+    // point(t, y2); // sin
+    sin_points.push([t, y2])
+    // point(y1, t); // cos
+    cos_points.push([y1, t])
+    for(let i = 0; i < sin_points.length; i++) {
+        x = sin_points[i][0];
+        y = sin_points[i][1];
+        point(x, y);
+    }
+
+    for(let i = 0; i < cos_points.length; i++) {
+        x = cos_points[i][0];
+        y = cos_points[i][1];
+        point(x, y);
+    }
+    // sin_points.append(point(t, y2));
+    // cos_points.append(point(y1, t));
+    pop();
+
 
 }
 
-function drawRadius(y, x) {
+function drawVector(y, x) {
+    push();
+    stroke("purple");
     line(0, 0, x, y);
     drawProjections(y, x);
+    pop();
 }
 
 function drawProjections(y, x) {
+    push();
+    stroke("yellow");
     line(x, 0, x, y);
     line(x, y, 0, y);
+    pop()
 }
 
 function drawOscillations(y, x) {
+    push();
+    stroke("blue");
+    fill("blue")
     circle(0, y, 5); // sin
     circle(x, 0, 5); // cos
+    pop();
 
 }
 
